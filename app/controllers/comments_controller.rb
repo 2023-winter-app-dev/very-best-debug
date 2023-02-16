fa94c1986939d3df08250df192885af37fa5c671
cwen-booth <cwen@chicagobooth.edu>
2023-02-16 01:09:57 +0000

    Finished in-class assignment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
   def create
     comment = Comment.new
-    comment.author_id = params.fetch("query_author_id")
-    comment.venue_id = params.fetch("query_venue_id")
-    comment.body = params.fetch("query_body")
+    comment.author_id = params.fetch("browser_author_id")
+    comment.venue_id = params.fetch("browser_venue_id")
+    comment.body = params.fetch("browser_comment")
     comment.save
-    
+
     redirect_to("/venues/#{comment.venue_id}")
   end
 end
